bd702d8bc7c48e1c2245fa85adaea01ebbfdc127
Jose E Moraes <elvano@google.com>
2024-10-25 20:44:29 -0300

    pre integracao 1

--- app/main.py
+++ app/main.py
@@ -20,7 +20,9 @@ app = FastAPI()
 init_db()  # Add this line
 init_question_log_db()
 
+
 app.middleware("http")(question_interceptor)
+
 
 origins = [
     "http://localhost:3000",  # Assuming your frontend is running on port 3000
@@ -28,14 +30,22 @@ origins = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8080",
 ]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # Allows all origins
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+logger.debug("CORS configurado com as origens permitidas: %s", "*")
 
 app.include_router(assistants_router, prefix="/api/v1", tags=["Assistants"])
 app.include_router(vector_store_router, prefix="/api/v1", tags=["Vector Stores"])
@@ -49,46 +59,7 @@ app.include_router(users_router, prefix="/api/v1", tags=["Users"])
 Base.metadata.create_all(bind=engine)
 logger.debug("Tabelas criadas no banco de dados.")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
-    allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
-)
-
-logger.debug("CORS configurado com as origens permitidas: %s", "*")
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info", reload=True)
-    
-    
-    
-# import os
-# from anthropic import Anthropic
-
-
-# client = anthropic.Anthropic(
-#     # defaults to os.environ.get("ANTHROPIC_API_KEY")
-#     api_key="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",
-# )
-
-
-# client = Anthropic(
-#     # This is the default and can be omitted
-#     api_key="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"
-# )
-
-# message = client.messages.create(
-#     max_tokens=1024,
-#     messages=[
-#         {
-#             "role": "user",
-#             "content": "Hello, Claude",
-#         }
-#     ],
-#     model="claude-3-opus-20240229",
-# )
-# print(message.content)
-# print(message.content)    
